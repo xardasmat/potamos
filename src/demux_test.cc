c12cc09a4aceebda22bf1d9ea6a6c5ce5b22d94f
mat <3667700+xardasmat@users.noreply.github.com>
2026-02-11 16:14:03 +0000

    use ipstream for reference streams in demux test instead of raw FILE

--- src/demux_test.cc
+++ src/demux_test.cc
@@ -14,6 +14,7 @@ extern "C" {
 
 #include "audio.hpp"
 #include "demux.hpp"
+#include "ipstream.hpp"
 
 namespace potamos {
 
@@ -30,8 +31,9 @@ TEST(DemuxTest, ReadBasicMp3File) {
   static const std::string cmd =
       "ffmpeg -i test_data/orders.mp3 -v quiet -f f32le -y -";
 
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
-                                                pclose);
+  iPipeStream pipe(cmd);
+  ASSERT_TRUE(pipe.good())
+      << " failed to initialize the pipeline for the reference stream";
 
   std::ifstream input_file(input_file_name);
   Demux demux(input_file);
@@ -40,15 +42,12 @@ TEST(DemuxTest, ReadBasicMp3File) {
   auto codec = demux.GetDecoder(0);
   AudioDecoder<float> audio_codec(codec);
 
-  ASSERT_FALSE(feof(pipe.get()));
-  ASSERT_FALSE(ferror(pipe.get()));
-
   int index = 0;
   float raw_float = 1337;
   const float start_time_seconds = 0.050113;
   while (auto sample = audio_codec.Read()) {
-    ASSERT_EQ(fread(&raw_float, 1, 4, pipe.get()), 4)
-        << "end of bytes at " << index;
+    pipe.read((char*)&raw_float, 4);
+    ASSERT_EQ(pipe.gcount(), 4) << "end of bytes at " << index;
     ASSERT_EQ(sample->sample(0), raw_float) << "samples mismatch at " << index;
 
     ASSERT_THAT(double(sample->time()),
@@ -57,8 +56,8 @@ TEST(DemuxTest, ReadBasicMp3File) {
     ++index;
   }
 
-  ASSERT_EQ(fread(&raw_float, 1, 4, pipe.get()), 0) << "some bytes at the end ";
-  EXPECT_TRUE(feof(pipe.get())) << "not all samples were decoded";
+  pipe.peek();
+  ASSERT_TRUE(pipe.eof()) << " there are still samples in the reference stream";
 }
 
 TEST(DemuxTest, ReadFlacFile) {
@@ -69,8 +68,9 @@ TEST(DemuxTest, ReadFlacFile) {
   static const std::string cmd =
       "ffmpeg -i test_data/orders.flac -v quiet -f s32le -y -";
 
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
-                                                pclose);
+  iPipeStream pipe(cmd);
+  ASSERT_TRUE(pipe.good())
+      << " failed to initialize the pipeline for the reference stream";
 
   std::ifstream input_file(input_file_name);
   Demux demux(input_file);
@@ -79,15 +79,12 @@ TEST(DemuxTest, ReadFlacFile) {
   auto codec = demux.GetDecoder(0);
   AudioDecoder<int> audio_codec(codec);
 
-  ASSERT_FALSE(feof(pipe.get()));
-  ASSERT_FALSE(ferror(pipe.get()));
-
   int index = 0;
   int32_t raw_sample = 1337;
   const float start_time_seconds = 0.0;
   while (auto sample = audio_codec.Read()) {
-    ASSERT_EQ(fread(&raw_sample, 1, 4, pipe.get()), 4)
-        << "end of bytes at " << index;
+    pipe.read((char*)&raw_sample, 4);
+    ASSERT_EQ(pipe.gcount(), 4) << "end of bytes at " << index;
     ASSERT_EQ(sample->sample(0), raw_sample)
         << "samples mismatch at " << index << " : " << std::hex
         << sample->sample(0) << " vs " << raw_sample;
@@ -98,9 +95,8 @@ TEST(DemuxTest, ReadFlacFile) {
     ++index;
   }
 
-  ASSERT_EQ(fread(&raw_sample, 1, 4, pipe.get()), 0)
-      << "some bytes at the end ";
-  EXPECT_TRUE(feof(pipe.get())) << "not all samples were decoded";
+  pipe.peek();
+  ASSERT_TRUE(pipe.eof()) << " there are still samples in the reference stream";
 }
 
 TEST(DemuxTest, ReadAacFile) {
@@ -111,8 +107,9 @@ TEST(DemuxTest, ReadAacFile) {
   static const std::string cmd =
       "ffmpeg -i test_data/orders.aac -v quiet -f f32le -y -";
 
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
-                                                pclose);
+  iPipeStream pipe(cmd);
+  ASSERT_TRUE(pipe.good())
+      << " failed to initialize the pipeline for the reference stream";
 
   std::ifstream input_file(input_file_name);
   Demux demux(input_file);
@@ -121,15 +118,12 @@ TEST(DemuxTest, ReadAacFile) {
   auto codec = demux.GetDecoder(0);
   AudioDecoder<float> audio_codec(codec);
 
-  ASSERT_FALSE(feof(pipe.get()));
-  ASSERT_FALSE(ferror(pipe.get()));
-
   int index = 0;
   float raw_sample = 1337;
   const float start_time_seconds = 0.0;
   while (auto sample = audio_codec.Read()) {
-    ASSERT_EQ(fread(&raw_sample, 1, 4, pipe.get()), 4)
-        << "end of bytes at " << index;
+    pipe.read((char*)&raw_sample, 4);
+    ASSERT_EQ(pipe.gcount(), 4) << "end of bytes at " << index;
     ASSERT_EQ(sample->sample(0), raw_sample)
         << "samples mismatch at " << index << " : " << std::hex
         << sample->sample(0) << " vs " << raw_sample;
@@ -140,9 +134,8 @@ TEST(DemuxTest, ReadAacFile) {
     ++index;
   }
 
-  ASSERT_EQ(fread(&raw_sample, 1, 4, pipe.get()), 0)
-      << "some bytes at the end ";
-  EXPECT_TRUE(feof(pipe.get())) << "not all samples were decoded";
+  pipe.peek();
+  ASSERT_TRUE(pipe.eof()) << " there are still samples in the reference stream";
 }
 
 TEST(DemuxTest, ReadStereoMp3File) {
@@ -150,8 +143,9 @@ TEST(DemuxTest, ReadStereoMp3File) {
   static const std::string cmd =
       "ffmpeg -i test_data/kirov.mp3 -v quiet -f f32le -y -";
 
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
-                                                pclose);
+  iPipeStream pipe(cmd);
+  ASSERT_TRUE(pipe.good())
+      << " failed to initialize the pipeline for the reference stream";
 
   std::ifstream input_file(input_file_name);
   Demux demux(input_file);
@@ -160,15 +154,12 @@ TEST(DemuxTest, ReadStereoMp3File) {
   auto codec = demux.GetDecoder(0);
   AudioDecoder<float> audio_codec(codec);
 
-  ASSERT_FALSE(feof(pipe.get()));
-  ASSERT_FALSE(ferror(pipe.get()));
-
   int index = 0;
   float raw_float[2] = {1, 2};
   const float start_time_seconds = 0.025057;
   while (auto sample = audio_codec.Read()) {
-    ASSERT_EQ(fread(raw_float, 1, 8, pipe.get()), 8)
-        << "end of bytes at " << index;
+    pipe.read((char*)raw_float, 8);
+    ASSERT_EQ(pipe.gcount(), 8) << "end of bytes at " << index;
     ASSERT_EQ(sample->sample(0), raw_float[0])
         << "samples mismatch at " << index;
     ASSERT_EQ(sample->sample(1), raw_float[1])
@@ -180,8 +171,8 @@ TEST(DemuxTest, ReadStereoMp3File) {
     ++index;
   }
 
-  ASSERT_EQ(fread(&raw_float, 1, 4, pipe.get()), 0) << "some bytes at the end ";
-  EXPECT_TRUE(feof(pipe.get())) << "not all samples were decoded";
+  pipe.peek();
+  ASSERT_TRUE(pipe.eof()) << " there are still samples in the reference stream";
 }
 
 }  // namespace potamos
